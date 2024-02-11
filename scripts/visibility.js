82defbb8cc6474d3488d4b49d4923c91cc5b572f
weydaej <emily.weyda+github@gmail.com>
2024-02-11 01:34:19 -0500

    file structure, margins and display only when everything is loaded

---
+++ scripts/visibility.js
@@ -0,0 +1,12 @@
+setTimeout(() => {
+  let domReady = (cb) => {
+    document.readyState === 'interactive' || document.readyState === 'complete'
+      ? cb()
+      : document.addEventListener('DOMContentLoaded', cb);
+  };
+
+  domReady(() => {
+    // Display body when DOM is loaded
+    document.body.style.visibility = 'visible';
+  });
+}, 200);
